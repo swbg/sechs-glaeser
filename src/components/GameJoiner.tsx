4e6c0530b92e64441286772ecef40fee1964915c
Stefan Weissenberger <weissenberger.stefan@bcg.com>
2021-02-03 23:40:36 +0100

    add game creator

---
+++ src/components/GameJoiner.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { joinGame as _joinGame } from "../firebase/interact";
+import { rtdb } from "../firebase/config";
+
+const GameJoiner = ({
+  setPid,
+  setGid,
+  setShowCreateGame,
+}: {
+  setPid: (pid: string) => void;
+  setGid: (gid: string) => void;
+  setShowCreateGame: (showCreateGame: boolean) => void;
+}) => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordDisabled, setPasswordDisabled] = useState(false);
+  const [nameError, setNameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  useEffect(() => {
+    if (window.location.search.startsWith("?game_id=")) {
+      setPasswordDisabled(true);
+      setPassword(window.location.search.substr("?game_id=".length));
+    }
+  }, []);
+
+  const handlePasswordError = () => {
+    setPassword("");
+    setPasswordError("Bitte eine existierende Spiel ID angeben!");
+    setPasswordDisabled(false);
+  }
+
+  const joinGame = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setNameError("");
+    setPasswordError("");
+    if (name.length < 3) {
+      setNameError("Bitte einen Namen mit mindestens 3 Buchstaben eingeben!");
+      return;
+    }
+    if (password.length < 3) {
+      setPasswordError("Bitte eine existierende Spiel ID angeben!");
+      return;
+    }
+    rtdb
+      .ref("games")
+      .child(password)
+      .once("value", snap => {
+        if (snap.val()) {
+          setGid(password);
+          const pid = _joinGame(name, password);
+          if (pid === null) {
+            setPasswordError("Unerwarteter Fehler beim Spielbeitritt aufgetreten.");
+          }
+          setPid(pid!);
+        } else {
+          handlePasswordError();
+        }
+      }, error => {
+        handlePasswordError();
+      });
+  };
+
+  return (
+    <div className="gameStarter">
+      <div className="container">
+        <form className="flexForm" onSubmit={joinGame}>
+          <label className="formLabel">Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="formInput nameField"
+          />
+          {nameError && <div className="errorField">{nameError}</div>}
+          {!passwordDisabled && <label className="formLabel">Spiel ID:</label>}
+          {!passwordDisabled && <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="formInput passwordField"
+            readOnly={passwordDisabled}
+          />}
+          {passwordError && <div className="errorField">{passwordError}</div>}
+          <button type="submit" className="startButton">Spiel beitreten</button>
+          {!passwordDisabled && <div className="spacer"></div>}
+          {!passwordDisabled && <button type="button" className="startButton" onClick={() => setShowCreateGame(true)}>Neues Spiel</button>}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default GameJoiner;
