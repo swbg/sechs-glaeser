9b54ef6a53953370a9a5446fe04cc677279982cc
Stefan Weissenberger <weissenberger.stefan@bcg.com>
2021-02-05 00:15:22 +0100

    add environment variables

---
+++ src/firebase/config.tsx
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const rtdb = firebase.database();
+const auth = firebase.auth()
+
+export { rtdb, auth };
